90c97a921f93451e2bff9b325a380ad8e2fe5860
azevedogustavo <48110661+azevedogustavo@users.noreply.github.com>
2021-06-29 10:23:48 -0300

    Add files via upload

---
+++ install-docker-ubuntu18.04.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+## install docker ubuntu 18.04
+
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
+sudo apt update
+sudo apt install docker-ce -y
+sudo usermod -aG docker ${USER}
+newgrp docker
+sudo systemctl enable docker
