d2cce48871ff220877d14e11e82fe47857200bd7
azevedogustavo <48110661+azevedogustavo@users.noreply.github.com>
2021-08-19 16:33:16 -0300

    upgarde curl and libcurl4

--- install-docker-ubuntu18.04.sh
+++ install-docker-ubuntu18.04.sh
@@ -2,6 +2,7 @@
 ## install docker ubuntu 18.04
 
 sudo apt update
+sudo apt-get --only-upgrade install curl libcurl4 -y
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
